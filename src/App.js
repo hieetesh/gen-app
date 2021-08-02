7162f926c54811f14ae24a786217f678b514b3b3
Hitesh Dharmadhikari <dharmadhikarihitesh@gmail.com>
2021-08-02 18:27:59 -0400

    Created login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-//import logo from './logo.svg';
 import './App.css';
-import Button from './components/button/button';
+import Form from './components/form/form';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hitesh is in Canada
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Button label="Sign in"/>
+      <Form/>
     </div>
   );
 }
---
+++ src/components/form/form.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './form.css'
+import logo from '../../Assets/portfolio_logo_hitesh.png';
+
+import Button from '../button/button';
+
+
+function Form(props) {
+  return (
+    <div className="wrapper fadeInDown">
+    <div id="formContent">
+        <h2 className="active"> Log In </h2>
+
+        <div className="fadeIn first">
+        <img src={logo}  id="icon" alt="User Icon" />
+        </div>
+
+        <form>
+            <input type="text" id="login-username" className="form-control"
+            name="username" placeholder="Enter username"
+            required autoFocus />
+
+            <input type="text" id="login-password" type="password" className="form-control"
+            name="password" placeholder="Enter password"
+            required />
+
+            <Button id="login-button" className="btn btn-success button-style" label="Login"/>
+        </form>
+    </div>
+    </div>
+  );
+}
+
+export default Form;
